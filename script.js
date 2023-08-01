2cb8a6939fd5ca8774afb0bde900c9f00b9dffc5
Galin <sao4449@abv.bg>
2023-08-01 18:31:38 +0300

    Add functionality to change the size of the grid

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 window.onload = () => {
-    makeGrid(size)
+    initialize(size)
 }
 
 let size = 20;
 
 const divContainer = document.querySelector('.container')
+const scrollbar = document.querySelector('.scroll')
+const textSize = document.querySelector('.textSize')
 
+
+scrollbar.onmousemove = () => changeSizeText(scrollbar.value)
+scrollbar.onchange = (value) => initialize(scrollbar.value)
+
+function changeSize(newValue) {
+  size = newValue
+}
+
+function initialize(value) {
+  clearGrid()
+  changeSize(value)
+  changeSizeText()
+  makeGrid(scrollbar.value)
+}
+
+function changeSizeText(){
+    textSize.innerHTML = size + ' x ' + size;
+}
 
 function makeGrid(size) {
     divContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -18,4 +38,8 @@ function makeGrid(size) {
       divContainer.appendChild(element);
     }
     
+}
+
+function clearGrid(){
+    divContainer.innerHTML = ''
 }
